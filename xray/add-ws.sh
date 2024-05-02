ab8053846514ea32885cfb16f7bc98fb0b9014fc
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2024-05-02 12:06:58 +0700

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -21,7 +21,7 @@ while true; do
     echo -e "\\E[0;41;36m      Add Vmess Account      \E[0m"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
-    read -rp "User: " -e user
+    read -rp "User name: " -e user
     CLIENT_EXISTS=$(grep -w "$user" /etc/xray/config.json | wc -l)
 
     if [[ $CLIENT_EXISTS -eq '1' ]]; then
@@ -37,86 +37,81 @@ while true; do
         continue
     fi
 
-    # Check if the number of IPs connected to the user exceeds 5
-    if [[ $(count_ips "$user") -ge 5 ]]; then
-        clear
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\\E[0;41;36m      Add Vmess Account      \E[0m"
-        echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo ""
-echo "Maximum number of allowed connections reached for this user."
-echo ""
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -n 1 -s -r -p "Press any key to back on menu"
-continue
-fi
-done
+    read -rp "Number of IPs (max 5): " -e num_ips
+    if [[ $num_ips -lt 1 || $num_ips -gt 5 ]]; then
+        echo "Number of IPs must be between 1 and 5."
+        continue
+    fi
 
-uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "Expired (days): " masaaktif
-exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
-sed -i '/#vmess$/a\### '"$user $exp"'\
-},{"id": "'"$uuid"'","alterId": '"0"',"email": "'"$user"'"' /etc/xray/config.json
-exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
-sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
-},{"id": "'"$uuid"'","alterId": '"0"',"email": "'"$user"'"' /etc/xray/config.json
-asu=$(cat<<EOF
-{
-"v": "2",
-"ps": "${user}",
-"add": "${domain}",
-"port": "443",
-"id": "${uuid}",
-"aid": "0",
-"net": "ws",
-"path": "/vmess",
-"type": "none",
-"host": "",
-"tls": "tls"
+    read -rp "Expiration months: " -e exp_months
+    if [[ ! $exp_months =~ ^[0-9]+$ ]]; then
+        echo "Invalid input. Please enter a valid number."
+        continue
+    fi
+
+    uuid=$(cat /proc/sys/kernel/random/uuid)
+    exp=$(date -d "+$exp_months months" +"%Y-%m-%d")
+    sed -i '/#vmess$/a\### '"$user $exp"'\
+    },{"id": "'"$uuid"'","alterId": '"0"',"email": "'"$user"'"' /etc/xray/config.json
+    sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
+    },{"id": "'"$uuid"'","alterId": '"0"',"email": "'"$user"'"' /etc/xray/config.json
+    asu=$(cat<<EOF
+    {
+    "v": "2",
+    "ps": "${user}",
+    "add": "${domain}",
+    "port": "443",
+    "id": "${uuid}",
+    "aid": "0",
+    "net": "ws",
+    "path": "/vmess",
+    "type": "none",
+    "host": "",
+    "tls": "tls"
 }
 EOF)
-ask=$(cat<<EOF
-{
-"v": "2",
-"ps": "${user}",
-"add": "${domain}",
-"port": "80",
-"id": "${uuid}",
-"aid": "0",
-"net": "ws",
-"path": "/vmess",
-"type": "none",
-"host": "",
-"tls": "none"
+    ask=$(cat<<EOF
+    {
+    "v": "2",
+    "ps": "${user}",
+    "add": "${domain}",
+    "port": "80",
+    "id": "${uuid}",
+    "aid": "0",
+    "net": "ws",
+    "path": "/vmess",
+    "type": "none",
+    "host": "",
+    "tls": "none"
 }
 EOF)
-grpc=$(cat<<EOF
-{
-"v": "2",
-"ps": "${user}",
-"add": "${domain}",
-"port": "443",
-"id": "${uuid}",
-"aid": "0",
-"net": "grpc",
-"path": "vmess-grpc",
-"type": "none",
-"host": "",
-"tls": "tls"
+    grpc=$(cat<<EOF
+    {
+    "v": "2",
+    "ps": "${user}",
+    "add": "${domain}",
+    "port": "443",
+    "id": "${uuid}",
+    "aid": "0",
+    "net": "grpc",
+    "path": "vmess-grpc",
+    "type": "none",
+    "host": "",
+    "tls": "tls"
 }
 EOF)
-vmess_base641=$(base64 -w 0 <<< $vmess_json1)
-vmess_base642=$(base64 -w 0 <<< $vmess_json2)
-vmess_base643=$(base64 -w 0 <<< $vmess_json3)
-vmesslink1="vmess://$(echo $asu | base64 -w 0)"
-vmesslink2="vmess://$(echo $ask | base64 -w 0)"
-vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
-systemctl restart xray > /dev/null 2>&1
-service cron restart > /dev/null 2>&1
-clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
-echo -e "\E[0;41;36m Vmess Account \E[0m" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+    vmess_base641=$(base64 -w 0 <<< $vmess_json1)
+    vmess_base642=$(base64 -w 0 <<< $vmess_json2)
+    vmess_base643=$(base64 -w 0 <<< $vmess_json3)
+    vmesslink1="vmess://$(echo $asu | base64 -w 0)"
+    vmesslink2="vmess://$(echo $ask | base64 -w 0)"
+    vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
+    systemctl restart xray > /dev/null 2>&1
+    service cron restart > /dev/null 2>&1
+    clear
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+    echo -e "\\E[0;41;36m        Vmess Account        \E[0m" | tee -a /etc/log-create-vmess.log
+    echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
 echo -e "Remarks : ${user}" | tee -a /etc/log-create-vmess.log
 echo -e "Domain : ${domain}" | tee -a /etc/log-create-vmess.log
 echo -e "Wildcard : (bug.com).${domain}" | tee -a /etc/log-create-vmess.log
@@ -133,9 +128,7 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "Link TLS : ${vmesslink1}" | tee -a /etc/log-create-vmess.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
 echo -e "Link none TLS : ${vmesslink2}" | tee -a /etc/log-create-vmess.log
-echo -e "\033[0;34m━━━━━━
-
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
 echo -e "Link gRPC : ${vmesslink3}" | tee -a /etc/log-create-vmess.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vmess.log
 echo -e "Expired On : $exp" | tee -a /etc/log-create-vmess.log
