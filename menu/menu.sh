72db607dc9d079f7e62a0b9201653f7273186cf4
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-05-22 11:47:29 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -140,7 +140,7 @@ case $opt in
 6) clear ; m-system ;;
 7) clear ; running ;;
 8) clear ; clearcache ;;
-11) clear ; m-backup ;;
+11) clear ; m-bkp ;;
 9) clear ; reboot ; /sbin/reboot ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu ;;
